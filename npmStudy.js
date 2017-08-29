ab51371ccca9556e068f3c5aa5fc2823aa4a9533
王硕 <wangshuo@jiedaibao.com>
2017-08-29 16:40:48 +0800

    修改

--- npmStudy.js
+++ npmStudy.js
@@ -1,5 +1,5 @@
 //踩坑 init npm name 不能有大写字母
-//好多文档写如不写entry point 就是index.js 文件是入口文件，但是实际并不是entry point: (npmStudy.js) 
+//好多文档写如不写ey point 就是index.js 文件是入口文件，但是实际并不是entry point: (npmStudy.js) 
 
 console.log('test');
-console.log('test1.1');
+console.log('testMaster');
